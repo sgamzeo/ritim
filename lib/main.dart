ac4e260a191b9f8c1512a2a287c83790042e6965
senaoz <ssenagamzee@gmail.com>
2025-05-26 01:10:48 +0300

    cache & localization initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ritim/product/localization/localization_delegates.dart';
+import 'package:ritim/product/localization/localization_service.dart';
+import 'package:ritim/product/services/cache_box_service.dart';
+import 'package:ritim/product/services/getx_manager.dart';
 
-void main() {
+void main() async {
+  await CacheBoxService.initBoxes();
+  await GetxManager.setupServices();
   runApp(const MyApp());
 }
 
@@ -8,7 +15,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      translations: AppLanguages(),
+      locale: Get.find<LocalizationService>().getLocale(),
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
