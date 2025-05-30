4949ca308d06a124ed9d66a7056ce01c7094b02a
senaoz <ssenagamzee@gmail.com>
2025-05-31 00:50:19 +0300

    dio & logger not initialized --fixed

--- lib/product/services/getx_manager.dart
+++ lib/product/services/getx_manager.dart
@@ -1,9 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 import 'package:ritim/product/localization/localization_service.dart';
 import 'package:ritim/product/services/cache_service.dart';
 
 class GetxManager {
   static Future<void> setupServices() async {
+    Get.put(Dio(), permanent: true);
+    Get.put(Logger(), permanent: true);
     Get.put(CacheService(), permanent: true);
     await Get.put(LocalizationService(), permanent: true).initLocale();
   }
