7f310364cc31907ada28b945a671972d8f4843bb
senaoz <ssenagamzee@gmail.com>
2025-05-31 00:52:01 +0300

    app colors & theme configurations

---
+++ lib/product/config/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ritim/product/constants/app_colors.dart';
+
+class ThemeManager {
+  static ThemeData get lightTheme => ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppColors.dividerColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor,
+      iconTheme: IconThemeData(color: AppColors.primaryDeepOrange),
+    ),
+    primaryTextTheme: _textTheme(AppColors.primaryGreyColor),
+  );
+
+  static TextTheme _textTheme(Color color) => TextTheme(
+    displayLarge: _textStyle(fontSize: 34, color: color),
+    displayMedium: _textStyle(fontSize: 25, color: color),
+    displaySmall: _textStyle(fontSize: 20, color: color),
+    bodyLarge: _textStyle(fontSize: 16, color: color),
+    bodyMedium: _textStyle(fontSize: 14, color: color),
+    bodySmall: _textStyle(fontSize: 12, color: color),
+  );
+
+  static TextStyle _textStyle({
+    required double fontSize,
+    double? letterSpacing,
+    required Color color,
+  }) {
+    return GoogleFonts.sourceSans3(
+      fontSize: fontSize.sp,
+      letterSpacing: letterSpacing ?? 0,
+      color: color,
+    );
+  }
+}
---
+++ lib/product/constants/app_colors.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryTeal = Color(0xFF309898);
+  static const Color primaryOrange = Color(0xFFFF9F00);
+  static const Color primaryDeepOrange = Color(0xFFF4631E);
+  static const Color primaryRed = Color(0xFFCB0404);
+
+  static const Color backgroundColor = Color(0xFFFBFBFE);
+  static const Color primaryGreyColor = Color(0xFF919191);
+  static Color textFieldDarkFillColor = Colors.black.withOpacity(.2);
+  static const Color textFieldFillColor = Color(0xFFF1F1F1);
+
+  static const Color hintColor = Color.fromARGB(255, 182, 182, 182);
+  static const Color widgetBackgroundColor = Color.fromARGB(255, 246, 246, 246);
+  static const Color lightGray = Color(0xF8F8F8F8);
+  static const Color black = Color(0xFF242424);
+  static const Color white = Color(0xffF2f2f2);
+
+  static const Color dividerColor = Color(0xFFF1F1F1);
+  static const grey1100Color = Color(0xFF0F0F0F);
+  static const grey1000Color = Color(0xFF181817);
+  static const grey900Color = Color(0xFF2A2A2A);
+  static const grey800Color = Color(0xFF343434);
+  static const grey700Color = Color(0xFF494949);
+  static const grey600Color = Color(0xFF686868);
+  static const grey500Color = Color(0xFF7D7D7D);
+  static const grey400Color = Color(0xFF919191);
+  static const grey300Color = Color(0xFFA5A5A5);
+  static const grey200Color = Color(0xFFC3C3C3);
+  static const grey100Color = Color(0xFFE1E1E1);
+  static const grey50Color = Color(0xFAFAFAFA);
+}
