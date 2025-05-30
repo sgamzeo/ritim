57e23d2333c7da8edc7baa74d573f96c1c469cc9
senaoz <ssenagamzee@gmail.com>
2025-05-31 00:54:10 +0300

    screen util included and initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:ritim/feature/views/pomodoro_view/pomodoro_view.dart';
+import 'package:ritim/product/config/theme.dart';
 import 'package:ritim/product/localization/localization_delegates.dart';
 import 'package:ritim/product/localization/localization_service.dart';
 import 'package:ritim/product/services/cache_box_service.dart';
@@ -15,28 +18,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      translations: AppLanguages(),
-      locale: Get.find<LocalizationService>().getLocale(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          translations: AppLanguages(),
+          locale: Get.find<LocalizationService>().getLocale(),
+          theme: ThemeManager.lightTheme,
+          home: PomodoroView(),
+        );
+      },
     );
   }
 }
