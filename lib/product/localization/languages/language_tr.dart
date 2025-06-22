997c160115234d757e408daa837ecffb0dcd73d3
senaoz <ssenagamzee@gmail.com>
2025-06-23 00:57:37 +0300

    Update localization keys and translation files for Turkish and English

--- lib/product/localization/languages/language_tr.dart
+++ lib/product/localization/languages/language_tr.dart
@@ -1,1 +1,7 @@
-final Map<String, String> trTR = {};
+import 'package:ritim/product/localization/localization_keys.dart';
+
+final Map<String, String> trTR = {
+  LocalizationKeys.startTextKey: 'Başlat',
+  LocalizationKeys.pauseTextKey: 'Duraklat',
+  LocalizationKeys.resetTextKey: 'Sıfırla',
+};
--- lib/product/localization/localization_keys.dart
+++ lib/product/localization/localization_keys.dart
@@ -1,1 +1,5 @@
-class LocalizationKeys {}
+class LocalizationKeys {
+  static const String startTextKey = 'startTextKey';
+  static const String pauseTextKey = 'pauseTextKey';
+  static const String resetTextKey = 'resetTextKey';
+}
